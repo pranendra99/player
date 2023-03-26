523331e9675fb94ff4fe3ec1aea052a812f74f8a
dandyraka <dandyraka13@gmail.com>
2023-03-27 02:24:46 +0700

    Fix if no more quality

--- player/player.js
+++ player/player.js
@@ -1,76 +1,44 @@
-var video = document.getElementById('video');
-var qualityWrapper = document.getElementById('quality-wrapper');
-var userAgent = navigator.userAgent.toLowerCase();
-
-//var qualityBtn = document.querySelector('#quality-btn');
-var qualityList = document.querySelector('#quality-list');
+const video = document.getElementById('video');
+const qualityList = document.querySelector('#quality-list');
 
 function playM3u8(url) {
     if (Hls.isSupported()) {
         video.volume = 0.3;
-        var hls = new Hls();
-        var m3u8Url = decodeURIComponent(url)
+        const hls = new Hls();
+        const m3u8Url = decodeURIComponent(url)
         hls.loadSource(m3u8Url);
-        hls.currentLevel = -1
-        hls.loadLevel = -1
+        hls.currentLevel = -1;
+        hls.loadLevel = -1;
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
-            var levels = hls.levels;
+            const levels = hls.levels;
             qualityList.innerHTML = '';
-            for (var i = 0; i < levels.length; i++) {
-                var level = levels[i];
-                console.log(level)
-                var listItem = document.createElement('li');
-                var levelBtn = document.createElement('button');
-                levelBtn.classList.add('btn', 'btn-danger');
-                levelBtn.textContent = level.height + 'p';
-                levelBtn.value = i;
-                listItem.style.listStyle = 'none';
-                listItem.appendChild(levelBtn);
-                qualityList.appendChild(listItem);
-                levelBtn.addEventListener('click', function () {
-                    hls.currentLevel = parseInt(this.value);
-                });
+            if(levels.length > 1){
+                for (let i = 0; i < levels.length; i++) {
+                    const level = levels[i];
+                    const listItem = document.createElement('li');
+                    const levelBtn = document.createElement('button');
+                    levelBtn.classList.add('btn', 'btn-danger');
+                    levelBtn.textContent = level.height + 'p';
+                    levelBtn.value = i;
+                    listItem.style.listStyle = 'none';
+                    listItem.appendChild(levelBtn);
+                    qualityList.appendChild(listItem);
+                    levelBtn.addEventListener('click', function () {
+                        hls.currentLevel = parseInt(this.value);
+                    });
+                }
             }
             video.play();
         });
-        document.title = url;
-
-        /*var qualityBtn = document.querySelector('#quality-btn');
-        var qualityList = document.querySelector('#quality-list');
-        qualityBtn.addEventListener('click', function () {
-            var levels = hls.levels;
-            qualityList.innerHTML = '';
-            for (var i = 0; i < levels.length; i++) {
-                var level = levels[i];
-                console.log(level)
-                var listItem = document.createElement('li');
-                var levelBtn = document.createElement('button');
-                levelBtn.classList.add('btn', 'btn-danger');
-                levelBtn.textContent = level.height + 'p';
-                levelBtn.value = i;
-                listItem.appendChild(levelBtn);
-                qualityList.appendChild(listItem);
-                levelBtn.addEventListener('click', function () {
-                    hls.currentLevel = parseInt(this.value);
-                    qualityList.style.display = 'none';
-                });
-            }
-
-            // toggle the display style between "block" and "none"
-            if (qualityList.style.display === 'none') {
-                qualityList.style.display = 'block';
-            } else {
-                qualityList.style.display = 'none';
-            }
-        });*/
+        document.title = "JKT48 Live - " + url;
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = url;
         video.addEventListener('canplay', function () {
             video.play();
         });
         video.volume = 0.3;
-        document.title = url;
+        document.title = "JKT48 Live - " + url;
     }
 }
 
@@ -105,9 +73,6 @@ function vidFullscreen() {
 }
 
 playM3u8(window.location.href.split("#")[1]);
-if (userAgent.indexOf('iphone') !== -1 || userAgent.indexOf('ipad') !== -1 || userAgent.indexOf('ipod') !== -1) {
-    //qualityWrapper.style.display = 'none';
-}
 $(window).on('load', function () {
     $('#video').on('click', function () { this.paused ? this.play() : this.pause(); });
     Mousetrap.bind('space', playPause);
