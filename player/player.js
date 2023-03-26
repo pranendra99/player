929d1d9d8d7afd6f73509d3517fd34c33f096e99
dandyraka <dandyraka13@gmail.com>
2023-03-26 20:48:50 +0700

    Update

--- player/player.js
+++ player/player.js
@@ -1,60 +1,89 @@
 var video = document.getElementById('video');
 
-function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
+function playM3u8(url) {
+    if (Hls.isSupported()) {
+        video.volume = 0.3;
+        var hls = new Hls();
+        var m3u8Url = decodeURIComponent(url)
+        hls.loadSource(m3u8Url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+        document.title = url;
+
+        var qualityBtn = document.querySelector('#quality-btn');
+        var qualityList = document.querySelector('#quality-list');
+        qualityBtn.addEventListener('click', function () {
+            var levels = hls.levels;
+            qualityList.innerHTML = '';
+            for (var i = 0; i < levels.length; i++) {
+                var level = levels[i];
+                var listItem = document.createElement('li');
+                var levelBtn = document.createElement('button');
+                levelBtn.classList.add('btn', 'btn-danger');
+                levelBtn.textContent = level.height + 'p';
+                levelBtn.value = i;
+                listItem.appendChild(levelBtn);
+                qualityList.appendChild(listItem);
+                levelBtn.addEventListener('click', function () {
+                    hls.currentLevel = parseInt(this.value);
+                    qualityList.style.display = 'none';
+                });
+            }
+
+            // toggle the display style between "block" and "none"
+            if (qualityList.style.display === 'none') {
+                qualityList.style.display = 'block';
+            } else {
+                qualityList.style.display = 'none';
+            }
+        });
+
+
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+        video.volume = 0.3;
+        document.title = url;
     }
-	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
-		});
-		video.volume = 0.3;
-		document.title = url;
-  	}
 }
 
 function playPause() {
-    video.paused?video.play():video.pause();
+    video.paused ? video.play() : video.pause();
 }
 
 function volumeUp() {
-    if(video.volume <= 0.9) video.volume+=0.1;
+    if (video.volume <= 0.9) video.volume += 0.1;
 }
 
 function volumeDown() {
-    if(video.volume >= 0.1) video.volume-=0.1;
+    if (video.volume >= 0.1) video.volume -= 0.1;
 }
 
 function seekRight() {
-    video.currentTime+=5;
+    video.currentTime += 5;
 }
 
 function seekLeft() {
-    video.currentTime-=5;
+    video.currentTime -= 5;
 }
 
 function vidFullscreen() {
     if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
     }
 }
 
-playM3u8(window.location.href.split("#")[1])
+playM3u8(window.location.href.split("#")[1]);
 $(window).on('load', function () {
-    $('#video').on('click', function(){this.paused?this.play():this.pause();});
+    $('#video').on('click', function () { this.paused ? this.play() : this.pause(); });
     Mousetrap.bind('space', playPause);
     Mousetrap.bind('up', volumeUp);
     Mousetrap.bind('down', volumeDown);
